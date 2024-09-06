33a49ed43ba788cf0d3a2bf98dc91efc8a7894e8
Murillo Ferraz <marteldelfer@gmail.com>
2024-09-05 21:29:08 -0300

    Create Spring API

---
+++ crudreact/src/main/java/com/marteldelfer/crudreact/CrudreactApplication.java
@@ -0,0 +1,13 @@
+package com.marteldelfer.crudreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudreactApplication.class, args);
+	}
+
+}
