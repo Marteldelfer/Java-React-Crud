822e5c2b75b012d09d01a8fa3b353127caf62a9e
Murillo Ferraz <marteldelfer@gmail.com>
2024-10-12 21:17:49 -0300

    post method frontend

--- reactCrud/src/App.jsx
+++ reactCrud/src/App.jsx
@@ -4,22 +4,7 @@ function App() {
 
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>quantity</th>
-            <th>price</th>
-            <th>createdAt</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Product/>
-        </tbody>
-      </table>
-      
+      <Product/>
     </>
   )
 }
--- reactCrud/src/Product.jsx
+++ reactCrud/src/Product.jsx
@@ -1,35 +1,104 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, act} from 'react';
 import axios from 'axios';
 
 function Product() {
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        axios.get('http://localhost:8080/api/')
-            .then(response => {
-                setData(response.data);
-            }).catch(error => console.error(error));
-    }, []);
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		axios.get('http://localhost:8080/api/')
+			.then(response => {
+				setData(response.data);
+			}).catch(error => console.error(error));
+	}, []);
 
-    function deleteProduct(id) {
-        axios.delete('http://localhost:8080/api/' + id)
-            .then(() => setData(d => d.filter(prod => prod.id !== id)));
-    }
+	const [nameInput, setNameInput] = useState("");
+	const [quantityInput, setQuantityInput] = useState();
+	const [priceInput, setPriceInput] = useState();	
 
-    return (
-        data.map(product =>
-        <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.name}</td>
-            <td>{product.quantity}</td>
-            <td>{product.price}</td>
-            <td>{product.createdAt.substring(0, 10)}</td>
-            <td>
-                <button className='delete' onClick={() => deleteProduct(product.id)}>Delete</button>
-                <button className='edit'>Edit</button>
-            </td>
-        </tr>
-        )
-    );
+	function handleNameChange(event) {
+		setNameInput(event.target.value);
+	}
+	function handleQuantityChange(event) {
+		setQuantityInput(event.target.value);
+	}
+	function handlePriceChange(event) {
+		setPriceInput(event.target.value);
+	}
+
+	const form = (
+		<>
+		<label id='name'>Name: </label>
+		<input type="text" value={nameInput} 
+			onChange={handleNameChange} placeholder='Product Name'/><br />
+		<label id='quantity'>Quantity: </label>
+		<input type="number" value={quantityInput} 
+			onChange={handleQuantityChange} placeholder='0'/><br />
+		<label id='price'>Price: </label>
+		<input type="number" value={priceInput} 
+			onChange={handlePriceChange} placeholder='0' /><br />
+		<button onClick={() => submitProduct()}>Create Product</button>
+		</>
+	);
+
+	function submitProduct() {
+
+		if (nameInput === "" || quantityInput === "" || priceInput === "") {
+			return;
+		}
+
+		const product = {
+			name: nameInput,
+			quantity: quantityInput,
+			price: priceInput
+		};
+
+		axios.post('http://localhost:8080/api/', product)
+			.then(response => {
+				setData(d => [...d, response.data]);
+				
+				setNameInput("");
+				setQuantityInput("");
+				setPriceInput("");
+			});
+		
+	};
+
+	function deleteProduct(id) {
+		axios.delete('http://localhost:8080/api/' + id)
+			.then(() => setData(d => d.filter(prod => prod.id !== id)));
+	}
+
+	return (
+		<>
+		{form}
+		<table>
+			<thead>
+				<tr>
+					<th>id</th>
+					<th>name</th>
+					<th>quantity</th>
+					<th>price</th>
+					<th>createdAt</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				{data.map(product =>
+				<tr key={product.id}>
+					<td>{product.id}</td>
+					<td>{product.name}</td>
+					<td>{product.quantity}</td>
+					<td>{product.price}</td>
+					<td>{product.createdAt.substring(0, 10)}</td>
+					<td>
+						<button className='delete' onClick={() => deleteProduct(product.id)}>Delete</button>
+						<button className='edit'>Edit</button>
+					</td>
+				</tr>
+				)}
+			</tbody>
+		</table>
+		</>
+	);
 }
 export default Product;
